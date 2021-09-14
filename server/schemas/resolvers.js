cc02a0259d81ff880b55609a1d51c02d972821d9
Mark S <marksherm04@gmail.com>
2021-09-14 13:45:41 -0400

    query in resolvers is written

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,27 @@
+const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+
 const resolvers = {
 	Query: {
-	  helloWorld: () => {
-		return 'Hello world!';
-	  }
+		me: async (parent, args, context) => {
+			if (context.user) {
+				const userInput = await User.findOne({ _id: context.user._id })
+					.select('-__v -password')
+
+				return userInput;
+			}
+			throw new AuthenticationError('Please log in!');
+		}
+	},
+
+	Mutation: {
+
 	}
-  };
-  
-  module.exports = resolvers;
+
+
+};
+
+
+module.exports = resolvers;
