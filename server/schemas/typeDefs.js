dcf1e5e440df02d844a054c795a8217dd340687c
Mark S <marksherm04@gmail.com>
2021-09-14 13:36:00 -0400

    typeDefs.js completed

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,53 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create typeDefs
+const typeDefs = gql`
+
+type Query {
+	me: User
+}
+
+type Mutation {
+	login(email: String!, password: String!): Auth
+	addUser(username: String!, email: String!, password: String!): Auth
+	saveBook(book: saveBook): User
+	removeBook(bookId: String!): User
+}
+
+type User {
+	_id: ID
+	username: String
+	email: String
+	bookCount: Int
+	savedBooks: [Book]
+}
+
+type Book {
+	bookId: ID
+	authors: [String]
+	description: String
+	title: String
+	image: String
+	link: String
+}
+
+type Auth {
+	token: ID!
+	user: User
+}
+
+input saveBook {
+	authors: [String]
+	description: String
+	title: String
+	bookId: String
+	image: String
+	link: String
+}
+
+}`;
+
+
+// export typeDefs
+module.exports = typeDefs;
--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
 const express = require('express');
+// import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+
+// import typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+// create new Apollo server and pass in schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
 
-app.use(express.urlencoded({ extended: true }));
+// integrate Apollo server with Express application as middleware
+server.applyMiddleware({ app });
+
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -17,5 +30,9 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    // log where we can go to test our GQL API
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
