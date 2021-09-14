c774a2974753fa9f2099e2c371fc586e451f0e9c
Mark S <marksherm04@gmail.com>
2021-09-14 14:48:06 -0400

    updated signupform and loginform to use mutations and imported react apollo/react-hooks

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,8 +1,9 @@
 // see SignupForm.js for comments
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/react-hooks';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { loginUser } from '../utils/API';
+import { LOGIN_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const LoginForm = () => {
