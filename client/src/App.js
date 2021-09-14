d6c60271595e5cebf03709147d94797e2c03ffd8
Mark S <marksherm04@gmail.com>
2021-09-14 14:44:18 -0400

    updated packages on client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import ApolloClient from 'apollo-boost';
+import { ApolloProvider, ApolloClient, InMemoryCache } from 'apollo-boost';
 import { ApolloProvider } from '@apollo/react-hooks';
 
 import SearchBooks from './pages/SearchBooks';
